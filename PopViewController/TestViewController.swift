9c2f171cc882a37ab6f62c7a6034a6f4248db3e2
Your NameZhangjingwang1993 <you@1005430006@qq.com>
2016-05-04 23:27:59 +0800

    仿淘宝 简书 app 推出View效果_Swift版本
    仿淘宝 简书 app 推出View效果_Swift版本

---
+++ PopViewController/TestViewController.swift
@@ -0,0 +1,77 @@
+//
+//  TestViewController.swift
+//  PopViewController
+//
+//  Created by Mac on 16/5/4.
+//  Copyright © 2016年 夏天. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: XtPopViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        let popView1 = UIView.init(frame: CGRectMake(0, screen_Height, screen_Width, screen_Height / 2))
+        /// popView1 是点击打开的时候下方弹出的view
+        popView1.backgroundColor = UIColor.grayColor()
+        /// 加个阴影
+        popView1.layer.shadowColor = UIColor.blackColor().CGColor
+        popView1.layer.shadowOffset = CGSizeMake(0.5, 0.5)
+        popView1.layer.shadowOpacity = 0.8
+        popView1.layer.shadowRadius = 5
+
+        let main = ViewController()
+        let mainNav = UINavigationController.init(rootViewController: main)
+        
+        /// 关闭按钮
+        let btnClose = UIButton.init(type: UIButtonType.Custom)
+        btnClose.frame = CGRectMake(15, 0, 50, 40)
+        btnClose.setTitle("Close", forState: UIControlState.Normal)
+        btnClose.setTitleColor(UIColor.cyanColor(), forState: UIControlState.Normal)
+        btnClose.addTarget(self, action:Selector("close"), forControlEvents: UIControlEvents.TouchUpInside)
+        popView1.addSubview(btnClose)
+
+        // 打开按钮
+        let btnOpen = UIButton.init(type: UIButtonType.Custom)
+        btnOpen.frame = CGRectMake(((screen_Width - 100) / 2), 300, 50, 40)
+        
+        btnOpen.setTitle("打开", forState: UIControlState.Normal)
+        btnOpen.setTitleColor(UIColor.cyanColor(), forState: UIControlState.Normal)
+        btnOpen.addTarget(self, action: Selector("open"), forControlEvents: UIControlEvents.TouchUpInside)
+        /// main.view 是主控制器的self.view
+        main.view.addSubview(btnOpen)
+        main.view.backgroundColor = UIColor.whiteColor()
+        main.title = "XTtest"
+        self.createPopViewControllerWithMainViewController(mainNav, popView: popView1)
+//        self.view.backgroundColor = UIColor.whiteColor() // 底部
+    }
+    func open()
+    {
+        print("+++++++++")
+        self.openAction()
+    }
+    func close()
+    {
+        self.closeAction()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PopViewController/XtPopViewController.swift
@@ -0,0 +1,136 @@
+//
+//  XtPopViewController.swift
+//  PopViewController
+//
+//  Created by zjwang on 5/4/16.
+//  Copyright © 2016 夏天. All rights reserved.
+//
+
+import UIKit
+let screen_Width = UIScreen.mainScreen().bounds.size.width
+let screen_Height = UIScreen.mainScreen().bounds.size.height
+class XtPopViewController: UIViewController {
+
+        /// 在底部弹出的View
+    var popView = UIView()
+        /// rootView
+    var rootView = UIView()
+        /// 主VC
+    var mainVc: UIViewController?
+        /// maskView
+    var maskView = UIView()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    func createPopViewControllerWithMainViewController(root: UIViewController, popView: UIView) -> Void {
+        
+        self.mainVc = root
+        self.popView = popView
+        self.createSubviews()
+    }
+    
+    func createSubviews() {
+        self.view.backgroundColor = UIColor.blackColor()
+        mainVc!.view.frame = self.view.bounds
+        mainVc!.view.backgroundColor = UIColor.grayColor()
+        rootView = mainVc!.view
+        self.addChildViewController(mainVc!)
+        self.view.addSubview(rootView)
+        
+    }
+    
+    func closeAction(){
+        var frame = popView.frame
+        frame.origin.y += popView.frame.size.height
+        
+        UIView.animateWithDuration(0.3, animations: {
+            //
+            self.maskView.alpha = 0
+            self.popView.frame = frame
+            // 改善滑动效果
+            self.rootView.layer.transform = self.firstTransform()
+            
+            }) { (Bool) in
+                UIView.animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+                    // 变为初始值
+                    self.rootView.layer.transform = CATransform3DIdentity;
+                    }, completion: { (Bool) in
+                        // 移除
+                        self.popView.removeFromSuperview()
+                })
+        }
+        
+        
+        
+    }
+    func openAction(){
+        UIApplication.sharedApplication().windows[0].addSubview(popView)
+        var frame = popView.frame
+        frame.origin.y = self.view.bounds.size.height - self.popView.frame.size.height
+        
+        UIView .animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { 
+            //
+            self.rootView.layer.transform = self.firstTransform()
+            }) { (Bool) in
+                //
+                UIView .animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { 
+                    //
+                    self.rootView.layer.transform = self.secondTransform()
+                    // 显示maskview, 模糊阴影
+                    self.maskView = UIView.init(frame: self.view.bounds)
+                    self.maskView.backgroundColor = UIColor.whiteColor()
+                    self.maskView.alpha = 0.5
+                    self.rootView.addSubview(self.maskView)
+                    // popView上升
+                    self.popView.frame = frame
+                    }, completion: { (Bool) in
+                        //
+                })
+        }
+        
+    }
+    func firstTransform() -> CATransform3D {
+        var t1 = CATransform3DIdentity;
+        t1.m34 = 1.0 / -900;
+        //带点缩小的效果
+        t1 = CATransform3DScale(t1, 0.95, 0.95, 1);
+        //绕x轴旋转
+        t1 = CATransform3DRotate(t1, 15.0 * (CGFloat)(M_PI)/180.0, 1, 0, 0);
+        return t1;
+    }
+    func secondTransform() -> CATransform3D {
+        var t2 = CATransform3DIdentity
+        t2.m34 = self.firstTransform().m34;
+        //向上移
+        t2 = CATransform3DTranslate(t2, 0, self.view.frame.size.height * (-0.08), 0);
+        //第二次缩小
+        t2 = CATransform3DScale(t2, 0.8, 0.8, 1);
+        return t2;
+    }
+    
+   
+
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
