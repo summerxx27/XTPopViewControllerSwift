fbddd295e287260ef52075d5fad2681dac6cc7f3
summerxx <summerxx27@163.com>
2022-12-01 01:24:33 +0800

    refactor: code review

--- PopViewController/TestViewController.swift
+++ PopViewController/TestViewController.swift
@@ -5,9 +5,11 @@
 //  Created by summerxx on 16/5/4.
 //  Copyright © 2016年 summerxx. All rights reserved.
 //
+
 /************************************************************** */
 /**** 文章你可以看我的简书   http://www.jianshu.com/p/01a420681ca9  */
 /************************************************************** */
+
 import UIKit
 
 class TestViewController: XTPopViewController {
@@ -15,56 +17,49 @@ class TestViewController: XTPopViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let popView1 = UIView.init(frame: CGRect.init(x: 0, y: screen_Height, width: screen_Width, height: 450))
-        /// popView1 是点击打开的时候下方弹出的view
-        popView1.backgroundColor = UIColor.red
-        /// 加个阴影
-        popView1.layer.shadowColor = UIColor.black.cgColor
-        popView1.layer.shadowOffset = CGSize.init(width: 0.5, height: 0.5)
-        popView1.layer.shadowOpacity = 0.8
-        popView1.layer.shadowRadius = 5
+        /// popView 是点击打开的时候下方弹出的view
+        let popView = UIView().then {
+            $0.frame = CGRect(x: 0, y: screen_Height, width: screen_Width, height: 450)
+            $0.backgroundColor = .red
+            $0.layer.shadowColor = UIColor.black.cgColor
+            $0.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+            $0.layer.shadowOpacity = 0.8
+            $0.layer.shadowRadius = 5
+        }
+
 
         let main = ViewController()
         let mainNav = UINavigationController.init(rootViewController: main)
         
         /// 关闭按钮
-        let btnClose = UIButton.init(type: UIButton.ButtonType.custom)
-        btnClose.frame = CGRect.init(x: 15, y: 0, width: 50, height: 40)
-        btnClose.setTitle("Close", for: UIControl.State.normal)
-        btnClose.setTitleColor(UIColor.cyan, for: UIControl.State.normal)
-        btnClose.addTarget(self, action:#selector(close), for: UIControl.Event.touchUpInside)
-        popView1.addSubview(btnClose)
+        let btnClose = UIButton().then {
+            $0.frame = CGRect(x: 15, y: 0, width: 50, height: 40)
+            $0.setTitle("Close", for: .normal)
+            $0.setTitleColor(.cyan, for: .normal)
+            $0.addTarget(self, action:#selector(close), for: .touchUpInside)
+        }
 
-        // 打开按钮
-        let btnOpen = UIButton.init(type: UIButton.ButtonType.custom)
-        btnOpen.frame = CGRect.init(x: 0, y: 0, width: 50, height: 40)
-        btnOpen.center = self.view.center;
-        btnOpen.setTitle("open", for: UIControl.State.normal)
-        btnOpen.setTitleColor(UIColor.cyan, for: UIControl.State.normal)
-        btnOpen.addTarget(self, action: #selector(open), for: UIControl.Event.touchUpInside)
-        /// main.view 是主控制器的self.view
+        popView.addSubview(btnClose)
+
+        let btnOpen = UIButton().then {
+            $0.frame = CGRect(x: 0, y: 0, width: 50, height: 40)
+            $0.center = self.view.center;
+            $0.setTitle("open", for: .normal)
+            $0.setTitleColor(UIColor.cyan, for: .normal)
+            $0.addTarget(self, action: #selector(openClick), for: .touchUpInside)
+        }
+
         main.view.addSubview(btnOpen)
-        main.view.backgroundColor = UIColor.white
+        main.view.backgroundColor = .white
         main.title = "summerxx"
-        self.createPopViewControllerWithMainViewController(root: mainNav, popView: popView1)
-        
-        
+        createPopViewController(rootVC: mainNav, bottomView: popView)
     }
     
-    @objc func open()
-    {
-        print("+++++++++")
+    @objc func openClick() {
         self.openAction()
     }
     
-    @objc func close()
-    {
+    @objc func close() {
         self.closeAction()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
--- PopViewController/XTPopViewController.swift
+++ PopViewController/XTPopViewController.swift
@@ -7,43 +7,49 @@
 //
 
 import UIKit
+
 let screen_Width = UIScreen.main.bounds.size.width
 let screen_Height = UIScreen.main.bounds.size.height
+
 class XTPopViewController: UIViewController {
 
-        /// 在底部弹出的View
-    var popView = UIView()
-        /// rootView
-    var rootView = UIView()
-        /// 主VC
-    var mainVc: UIViewController?
-        /// maskView
-    var maskView = UIView()
-    func createPopViewControllerWithMainViewController(root: UIViewController, popView: UIView) -> Void {
-        
-        self.mainVc = root
-        self.popView = popView
-        self.createSubviews()
+    /// 在底部弹出的View
+    private var popView = UIView()
+
+    /// rootView
+    private var rootView = UIView()
+
+    /// 主VC
+    private var mainVC: UIViewController?
+
+    /// maskView
+    private var maskView = UIView()
+
+    func createPopViewController(rootVC: UIViewController, bottomView: UIView) -> Void {
+        mainVC = rootVC
+        popView = bottomView
+        setupSubviews()
     }
-    
-    func createSubviews() {
-        self.view.backgroundColor = UIColor.black
-        mainVc!.view.frame = self.view.bounds
-        rootView = mainVc!.view
-        self.addChild(mainVc!)
-        self.view.addSubview(rootView)
-        
+
+    private func setupSubviews() {
+        view.backgroundColor = .black
+        guard let vc = mainVC else { return }
+        vc.view.frame = self.view.bounds
+        rootView = vc.view
+        addChild(vc)
+        view.addSubview(rootView)
     }
-    
-    func closeAction(){
+
+    func closeAction() {
         var frame = popView.frame
+
         frame.origin.y += popView.frame.size.height
-        
+
         UIView.animate(withDuration: 0.3, animations: {
             self.popView.frame = frame
             // 改善滑动效果
             self.rootView.layer.transform = self.firstTransform()
-            
+
             }) { (Bool) in
                 UIView.animate(withDuration: 0.3, delay: 0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
                     // 变为初始值
@@ -53,61 +59,46 @@ class XTPopViewController: UIViewController {
                         self.popView.removeFromSuperview()
                 })
         }
-        
-        
-        
     }
-    
+
     func openAction(){
         UIApplication.shared.windows[0].addSubview(popView)
         var frame = popView.frame
         frame.origin.y = self.view.bounds.size.height - self.popView.frame.size.height
-        
+
         UIView.animate(withDuration: 0.3, delay: 0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
-            
+
             self.rootView.layer.transform = self.firstTransform()
             }) { (Bool) in
-                
+
                 UIView.animate(withDuration: 0.3, delay: 0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
-                    
+
                     self.rootView.layer.transform = self.secondTransform()
                     // popView上升
                     self.popView.frame = frame
                     }, completion: { (Bool) in
                 })
         }
-        
+
     }
-    
-    func firstTransform() -> CATransform3D {
+
+    private func firstTransform() -> CATransform3D {
         var t1 = CATransform3DIdentity;
         t1.m34 = 1.0 / -900;
-        //带点缩小的效果
+        // 带点缩小的效果
         t1 = CATransform3DScale(t1, 0.95, 0.95, 1);
-        //绕x轴旋转
-        t1 = CATransform3DRotate(t1, 15.0 * (CGFloat)(M_PI)/180.0, 1, 0, 0);
+        // 绕x轴旋转
+        t1 = CATransform3DRotate(t1, 15.0 * (CGFloat)(Double.pi) / 180.0, 1, 0, 0);
         return t1;
     }
-    
-    func secondTransform() -> CATransform3D {
+
+    private func secondTransform() -> CATransform3D {
         var t2 = CATransform3DIdentity
         t2.m34 = self.firstTransform().m34;
-        //向上移
+        // 向上移
         t2 = CATransform3DTranslate(t2, 0, self.view.frame.size.height * (-0.08), 0);
-        //第二次缩小
+        // 第二次缩小
         t2 = CATransform3DScale(t2, 0.8, 0.8, 1);
         return t2;
-    }
-    
-   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
